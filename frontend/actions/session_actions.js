87e5c8eccae77f3ac3a730806454e54be883ce03
Drew Nguyen <drew.nguyen5@gmail.com>
2017-10-23 18:41:26 -0400

    Session and User frontend actions, closes #55

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,42 @@
+import * as SessionAPI from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = user => (
+  {
+    type: RECEIVE_CURRENT_USER,
+    user
+  }
+);
+
+export const receiveErrors = errors => (
+  {
+    type: RECEIVE_ERRORS,
+    errors
+  }
+);
+
+export const login = user => dispatch => (
+  SessionAPI.login(user)
+    .then(
+      (currentUser) => dispatch(receiveCurrentUser(currentUser)),
+      (errors) => dispatch(receiveErrors(errors))
+    )
+);
+
+export const signup = user => dispatch => (
+  SessionAPI.signup(user)
+    .then(
+      (currentUser) => dispatch(receiveCurrentUser(currentUser)),
+      (errors) => dispatch(receiveErrors(errors))
+    )
+);
+
+export const logout = () => dispatch => (
+  SessionAPI.logout()
+    .then(
+      () => dispatch(receiveCurrentUser(null)),
+      (errors) => dispatch(receiveErrors(errors))
+    )
+);
