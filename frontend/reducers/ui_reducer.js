d78e900ac7d008b85caf2782ee6347bd3e71a416
Drew Nguyen <drew.nguyen5@gmail.com>
2017-10-24 16:52:34 -0400

    Added ui action types, no ui action creators yet

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,23 +1,20 @@
-import { RECEIVE_PROJECTS,
-         RECEIVE_PROJECT,
-         REMOVE_PROJECT } from '../actions/project_actions';
+import { OPEN_MODAL,
+         CLOSE_MODAL, } from '../actions/ui_actions';
 import merge from 'lodash/merge';
 
-const defaultState = {};
+const defaultState = {
+  modal: null,
+};
 
-const UIReducer = (state = {}, action) => {
+const UIReducer = (state = defaultState, action) => {
   console.log(action.type);
   Object.freeze(state);
   let newState;
   switch (action.type) {
-    case RECEIVE_PROJECTS:
+    case OPEN_MODAL:
       return merge({}, state, action.projects);
-    case RECEIVE_PROJECT:
-      return merge({}, state, { [action.project.id]: action.project });
-    case REMOVE_PROJECT:
-      newState = merge({}, state);
-      delete newState[action.projectId];
-      return newState;
+    case CLOSE_MODAL:
+      return merge({}, state, { modal: null });
     default:
       return merge({}, state);
   }
