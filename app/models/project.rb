de0737032362f462d96f0983ef1b4459531c9128
Drew Nguyen <drew.nguyen5@gmail.com>
2017-10-24 12:15:18 -0400

    Project and User models, added association, closes #34

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+  validates :name, :owner_id, presence: true
+
+  belongs_to :owner,
+             class_name: "User",
+             foreign_key: :owner_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,10 @@ class User < ApplicationRecord
 
   attr_reader :password
 
+  has_many :projects,
+           class_name: "Project",
+           foreign_key: :owner_id
+
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
     user && user.is_password?(password) ? user : nil
