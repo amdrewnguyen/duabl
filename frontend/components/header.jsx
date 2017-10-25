5f3f3c8e365adc610d08544b2c5dd61b33863077
Drew Nguyen <drew.nguyen5@gmail.com>
2017-10-25 16:24:01 -0400

    Set Login form to render if not logged in

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { logout } from '../actions/session_actions';
@@ -23,10 +23,7 @@ const Header = ({ loggedIn, currentUser, logout }) => (
     <span>{currentUser.username}</span>
     <button onClick={logout}>Logout</button>
   </div> :
-  <div>
-    <Link to='/signup'>Sign Up </Link>
-    <Link to='/login'>Log In </Link>
-  </div>
+  <Redirect to="/login"/>
 );
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
