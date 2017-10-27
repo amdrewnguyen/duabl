2772ef0323018fe27c5e8e14e93eadf8d7ce4ceb
Drew Nguyen <drew.nguyen5@gmail.com>
2017-10-27 09:51:58 -0400

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 only_user = User.new(email: 'a@a.com', password: 'aaaaaa')
+only_other_user = User.new(email: 'demo', password: 'demooo')
 
 only_user.save
+only_other_user.save
 
 project_names = ["full stack", "test project", "build a spaceship",
   "make my bed", "take over the world", "get groceries"]
@@ -17,18 +20,21 @@ projects = []
 
 project_names.each do |name|
   projects.push(Project.create(name: name, owner_id: only_user.id))
+  projects.push(Project.create(name: name, owner_id: only_other_user.id))
 end
 
-verbs = %w(scold charge choke unlock mourn cough trace
+verbs = %w(scold charge unlock cough trace
            fasten guess claim joke license search challenge taste trick
            connect arrange avoid tap nest curve crush greet peep turn
-           screw memorise change follow sc waste coil ruin launch wave
-           stain drip refuse suit jump)
+           memorise change follow sc coil launch wave
+           stain suit jump)
 
 tasks = []
 
 projects.each do |proj|
-  (3 + rand(3)).times do
-    tasks.push(Task.create(name: verbs.sample, owner_id: only_user.id, project_id: proj.id))
+  (3 + rand(5)).times do
+    tasks.push(Task.create(name: Faker::Dune.unique.quote, owner_id: only_user.id, project_id: proj.id))
   end
 end
+
+puts "seed run successfully"
