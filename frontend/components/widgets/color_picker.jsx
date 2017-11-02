a5d8ee343e4b53dc8693a338aacd1f214764f83a
Drew Nguyen <drew.nguyen5@gmail.com>
2017-11-01 21:13:33 -0400

    Started color picker

---
+++ frontend/components/widgets/color_picker.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import enhanceWithClickOutside from 'react-click-outside';
+
+class ColorPicker extends React.Component {
+
+}
+
+export default enhanceWithClickOutside(ColorPicker);
